89b74a65a9b6ed646a65db165df109cb2637a13b
ZE0TRON <cmpbilge@gmail.com>
2018-02-21 20:36:32 +0300

    Folders cleaned and organised ssps.py solution fixed

---
+++ On_Eleme/SSPS/ssps.py
@@ -0,0 +1,35 @@
+import math
+
+
+def find_asals(N):
+    a = []
+    a.append(1)
+    for elem in range(2, N):
+        flag = True
+        for i in range(2, math.ceil(math.sqrt(elem)) + 1):
+            if (elem % i == 0):
+                flag = False
+        if (flag == True):
+            a.append(elem - 1)
+    return a
+
+
+N = int(input())
+dizi = list(map(int, input().split(" ")))
+aranan = int(input())
+asallar = find_asals(N)
+i = 0
+while i < len(asallar):
+    if (len(dizi) <= asallar[i]):
+        print("Not Found")
+        break
+    if (dizi[asallar[i]] < aranan):
+        nothing = 0
+    elif (dizi[asallar[i]] == aranan):
+        print("Found")
+        break
+    else:
+        dizi = dizi[asallar[i - 1]:asallar[i]]
+        print(*dizi)
+        i = -1
+    i += 1
