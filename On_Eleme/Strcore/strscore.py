ef0eba5b8f588d60215d868c8d56a17436428654
Ayca Meric Celik <aycameric.celik@gmail.com>
2018-02-26 03:01:07 +0300

    The problem input generator -kinda- written,solution is almost ready, just need one more function maybe,gl hf  xoxo

---
+++ On_Eleme/Strcore/strscore.py
@@ -0,0 +1,101 @@
+# Python program for insert and search
+# operation in a Trie
+from collections import deque
+
+class TrieNode:
+
+    # Trie node class
+    def __init__(self):
+        self.children = [None]*26
+
+        # isEndOfWord is True if node represent the end of the word
+        self.isEndOfWord = False
+        self.score = 1
+        self.char = None
+
+    def initializeScore(self):
+        for i in range(26):
+            child = self.children[i]
+            if(child is not None):
+                self.score += child.initializeScore()
+        return self.score
+
+    def traverse(self):
+        print("current char and score: ", self.char, self.score)
+        for i in range(26):
+            child = self.children[i]
+            if(child is not None):
+                child.traverse()
+
+class Trie:
+
+    # Trie data structure class
+    def __init__(self):
+        self.root = self.createNode()
+
+    def createNode(self):
+
+        # Returns new trie node (initialized to NULLs)
+        return TrieNode()
+
+    def _charToIndex(self,ch):
+
+        # private helper function
+        # Converts key current character into index
+        # use only 'a' through 'z' and lower case
+
+        return ord(ch)-ord('a')
+
+
+    def insert(self,key):
+
+        # If not present, inserts key into trie
+        # If the key is prefix of trie node,
+        # just marks leaf node
+        pCrawl = self.root
+        length = len(key)
+        for level in range(length):
+            index = self._charToIndex(key[level])
+            # if current character is not present
+            if not pCrawl.children[index]:
+                pCrawl.children[index] = self.createNode()
+                pCrawl.children[index].char = key[level]
+            pCrawl = pCrawl.children[index]
+
+        # mark last node as leaf
+        pCrawl.isEndOfWord = True
+
+    def calculateScore(self, key):
+        pCrawl = self.root
+        length = len(key)
+        score = 0
+        for level in range(length):
+            index = self._charToIndex(key[level])
+            pCrawl = pCrawl.children[index]
+            score += pCrawl.score
+        return score
+
+
+# driver function
+def main():
+
+    # Input keys (use only 'a' through 'z' and lower case)
+    keys = ["arabalar", "araba", "ara", "arama", "alma", "al", "almak"]
+
+    # Trie object
+    t = Trie()
+
+    # Construct trie
+    for key in keys:
+        t.insert(key)
+
+    t.root.initializeScore()
+    #t.root.traverse()
+
+    for key in keys:
+        print("current key:" ,key, "current score ", t.calculateScore(key))
+
+
+
+if __name__ == '__main__':
+    main()
