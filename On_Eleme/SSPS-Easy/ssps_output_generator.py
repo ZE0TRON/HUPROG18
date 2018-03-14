63cf1814d8287adff95ef384dc1d4e4ee4cc0b8b
Bilge Çimen <cmpbilge@gmail.com>
2018-03-14 18:39:12 +0300

    fixed ssps output generator and "pilge aglama"

--- On_Eleme/SSPS-Easy/ssps_output_generator.py
+++ On_Eleme/SSPS-Easy/ssps_output_generator.py
@@ -7,7 +7,7 @@ from time import time
 def find_asals(N):
     a = []
     a.append(1)
-    for elem in range(2, N):
+    for elem in range(2, N+1):
         flag = True
         for i in range(2, math.ceil(math.sqrt(elem)) + 1):
             if (elem % i == 0):
@@ -15,6 +15,22 @@ def find_asals(N):
         if (flag == True):
             a.append(elem - 1)
     return a
+
+def solve(arr,asals):
+    i=0
+    "print(asals,'asals')"
+    lnA=len(asals)
+    while i<lnA:
+        if(arr[asals[i]]==aranan):
+            return "Found"
+        if(arr[asals[i]]>aranan):
+            print(arr[asals[i-1]:asals[i]+1], file=out)
+            return solve(arr[asals[i-1]:asals[i]+1],asals[:i])
+        if(i+1==lnA):
+            print(arr[asals[i]+1:], file=out)
+            return solve(arr[asals[i]:],find_asals(len(arr[asals[i]:])))
+        i+=1
+    return "Not Found"
 
 a = time()
 inputs = os.listdir("./input")
@@ -28,19 +44,6 @@ for inp in inputs:
     dizi = list(map(int, inputfile.readline().strip().split(" ")))
     aranan = int(inputfile.readline())
     asallar = find_asals(N)
-    i = 0
-    while i < len(asallar):
-        if (len(dizi) <= asallar[i]-1):
-            print("Not Found",file=out)
-            break
-        if (dizi[asallar[i]-1] < aranan):
-            nothing = 0
-        elif (dizi[asallar[i]-1] == aranan):
-            print("Found",file=out)
-            break
-        else:
-            dizi = dizi[asallar[i-1]-1:asallar[i]-1]
-            print(*dizi,file=out)
-            i = 1
-        i += 1
+    print(solve(dizi, asallar), file=out)
+
     print(time()-a)
