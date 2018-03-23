4632192bbf8f3dc9a62ed1bd1fe51a973b994f36
BatuhanHU <batuhan@hacettepe.edu.tr>
2018-03-23 20:18:24 +0300

    fixed nukleer question text, added nukleer input generator, added nukleer source code

---
+++ Final/Expert-Patu/main.cpp
@@ -0,0 +1,337 @@
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<ctime>
+#include<cmath>
+#include<iostream>
+#include<sstream>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<map>
+#include<bitset>
+#include<string>
+#include<queue>
+#include<iomanip>
+#include<limits>
+#include<typeinfo>
+#include<functional>
+#include<numeric>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<ld,ld> pdd;
+
+#define X first
+#define Y second
+
+const int MaxN=300010;
+
+int n;
+
+struct point
+{
+    int x,y;
+    point () {}
+    point (int a,int b)
+    {
+        x=a;
+        y=b;
+    }
+    bool operator < (point A) const
+    {
+        return x<A.x||x==A.x&&y<A.y;
+    }
+    point operator - (point A)
+    {
+        return point(x-A.x,y-A.y);
+    }
+};
+
+map<point,int> s;
+map<int,map<point,int>> g;
+
+int m;
+
+struct line
+{
+    point a,b,v;
+    int from,id;
+    line () {}
+    line (point x,point y,int k=-1,int r=-1)
+    {
+        a=x;
+        b=y;
+        if (b<a) swap(a,b);
+        v=b-a;
+        from=k;
+        id=r;
+    }
+    ld get(ld X0)
+    {
+        return a.y+(X0-a.x)*v.y/v.x;
+    }
+};
+
+vector<line> w;
+
+set<int> event;
+map<int,vector<int>> inp,oup;
+
+ld X0;
+
+inline bool cmp(int i,int j)
+{
+    return w[i].get(X0)<w[j].get(X0);
+}
+
+inline int random(int x)
+{
+    return (rand()<<15|rand())%x;
+}
+
+struct node
+{
+    node *Lc,*Rc;
+    int id;
+    int size;
+    node ();
+    node (int);
+    node (int,node*,node*);
+    node (node*);
+};
+
+typedef pair<node*,node*> pnn;
+
+node *nil;
+
+inline node::node()
+{
+    Lc=Rc=nil;
+    id=-1;
+    size=1;
+}
+
+inline node::node (int p)
+{
+    Lc=Rc=nil;
+    id=p;
+    size=1;
+}
+
+inline node::node (int p,node *L,node *R)
+{
+    Lc=L;
+    Rc=R;
+    id=p;
+    size=L->size+R->size+1;
+}
+
+inline node::node (node *p)
+{
+    *this=*p;
+}
+
+node *merge(node *a,node *b)
+{
+    if (a==nil&&b==nil) return nil;
+    if (a==nil) return new node(b);
+    if (b==nil) return new node(a);
+    if (random(a->size+b->size)<a->size)
+        return new node(a->id,a->Lc,merge(a->Rc,b));
+    else
+        return new node(b->id,merge(a,b->Lc),b->Rc);
+}
+
+pnn split(node *a,int n)
+{
+    if (a==nil) return pnn(nil,nil);
+    if (!n) return pnn(nil,new node(a));
+    int cnt=a->Lc->size;
+    if (cnt>=n)
+    {
+        pnn p=split(a->Lc,n);
+        return pnn(p.X,new node(a->id,p.Y,a->Rc));
+    }
+    else
+    {
+        pnn p=split(a->Rc,n-cnt-1);
+        return pnn(new node(a->id,a->Lc,p.X),p.Y);
+    }
+}
+
+node *ins(node *r,int p)
+{
+    node *x=r;
+    int cnt=0;
+    while (x!=nil)
+    {
+        if (cmp(x->id,p))
+        {
+            cnt+=x->Lc->size+1;
+            if (x->Rc==nil) break;
+            x=x->Rc;
+        }
+        else
+        {
+            if (x->Lc==nil) break;
+            x=x->Lc;
+        }
+    }
+    pnn d=split(r,cnt);
+    x=new node(p);
+    return merge(merge(d.X,x),d.Y);
+}
+
+node *del(node *r,int p)
+{
+    node *x=r;
+    int cnt=0;
+    while (x!=nil)
+    {
+        if (x->id==p)
+        {
+            cnt+=x->Lc->size;
+            break;
+        }
+        if (cmp(x->id,p))
+        {
+            cnt+=x->Lc->size+1;
+            if (x->Rc==nil) break;
+            x=x->Rc;
+        }
+        else
+        {
+            if (x->Lc==nil) break;
+            x=x->Lc;
+        }
+    }
+    pnn a=split(r,cnt);
+    pnn b=split(a.Y,1);
+    return merge(a.X,b.Y);
+}
+
+int solve(node *r,int x,int y)
+{
+    int cnt=0;
+    node *p=nil;
+    while (r!=nil)
+    {
+        if (w[r->id].get(x)<=y)
+        {
+            p=r;
+            cnt+=r->Lc->size+1;
+            if (r->Rc==nil) break;
+            r=r->Rc;
+        }
+        else
+        {
+            if (r->Lc==nil) break;
+            r=r->Lc;
+        }
+    }
+    if (p==nil)
+        return -1;
+    if (w[p->id].get(x)==y||cnt%2)
+        return w[p->id].from;
+    return -1;
+}
+
+map<int,node*> root;
+
+int MINX,MAXX;
+
+void init()
+{
+    cin>>m;
+    for (int i=1;i<=m;++i)
+    {
+        int k;
+        scanf("%d",&k);
+        vector<point> v;
+        for (int j=0;j<k;++j)
+        {
+            int x,y;
+            scanf("%d%d",&x,&y);
+            point p(x,y);
+            v.push_back(p);
+            s[p]=i;
+        }
+        for (int j=0;j<k;++j)
+            w.push_back(line(v[j],v[(j+1)%k],i,m++));
+    }
+    for (auto p : w)
+        if (p.a.x<p.b.x)
+        {
+            event.insert(p.a.x);
+            inp[p.a.x].push_back(p.id);
+            event.insert(p.b.x);
+            oup[p.b.x].push_back(p.id);
+        }
+        else
+            g[p.a.x][point(p.a.y,p.b.y)]=p.from;
+    MINX=*event.begin();
+    MAXX=*event.rbegin();
+    nil=new node;
+    nil->Lc=nil->Rc=nil;
+    nil->size=0;
+    node *tmp=nil;
+    for (auto x : event)
+    {
+        for (auto p : oup[x])
+            tmp=del(tmp,p);
+        auto pp=event.find(x);
+        auto xx=pp;
+        ++xx;
+        X0=(x+*xx)/2.;
+        for (auto p : inp[x])
+            tmp=ins(tmp,p);
+        root[x]=tmp;
+    }
+}
+
+const int INF=1000000010;
+
+int check(int x,int y)
+{
+    point p(x,y);
+    if (s.count(p)) return s[p];
+    auto it=g[x].upper_bound(point(y,INF));
+    if (it!=g[x].begin())
+    {
+        --it;
+        if (it->X.x<=y&&y<=it->X.y) return it->Y;
+    }
+    if (MINX<=x&&x<=MAXX)
+    {
+        auto it=root.upper_bound(x);
+        --it;
+        return solve(it->Y,x,y);
+    }
+    return -1;
+}
+
+void work()
+{
+    int T;
+    cin>>T;
+    while (T--)
+    {
+        int x,y;
+        scanf("%d%d",&x,&y);
+        printf("%d\n",check(x,y));
+        fflush(stdout);
+    }
+}
+
+int main()
+{
+    init();
+    work();
+    return 0;
+}
---
+++ Final/Expert-Patu/nukleer_input_generator.py
@@ -0,0 +1,139 @@
+from random import randint
+from random import shuffle
+
+print("olusturulacak input case sayisini giriniz:")
+
+for i in range(int(input())):
+    if i<10:
+        out = open("input/input0" + str(i) + ".txt", "w")
+    else:
+        out = open("input/input" + str(i) + ".txt", "w")
+
+    print(i," numarali input icin  M adet ulke sayisini sayisini girin:")
+    M = int(input())
+
+    print(i, " numarali input icin  N adet nukleer sayisini sayisini girin:")
+    N = int(input())
+
+    sinir = 10000000
+
+    kok = M ** 0.5
+    kok = kok // 1 + 1
+    arttirma = sinir // kok - 1
+
+    xSinir = arttirma
+    ySinir = arttirma
+
+    #ulke olusturucu
+
+    print(M,file=out)
+    for _ in range(M):
+        if _ % 100 == 0:
+            print(_)
+        merkezX = randint(xSinir - arttirma + 3, xSinir - 3)
+        merkezY = randint(ySinir - arttirma + 3, ySinir - 3)
+        #print("merkez x ve y", merkezX,merkezY)
+        #print("sinir x ve y", xSinir, ySinir)
+
+        xList = []
+        yList = []
+
+        ustAlt = randint(0,1)
+        noktaX = merkezX
+        noktaY = merkezY
+
+        #print(noktaX,noktaY)
+        xList.append(noktaX)
+        yList.append(noktaY)
+
+        #uste uzanan sekil
+        if ustAlt == 0:
+            #sag taraftaan yukari cikma
+
+            while True:
+                if (randint(0,99) == 99 and noktaX != merkezX and noktaY != merkezY) or noktaY+1+((ySinir-1-noktaY)//100+1) > ySinir - 1:
+                    break
+
+                noktaX = randint(merkezX + 1,xSinir - 1)
+                noktaY = randint(noktaY+1,noktaY+1+((ySinir-1-noktaY)//100+1))
+
+                #print(noktaX,noktaY)
+                xList.append(noktaX)
+                yList.append(noktaY)
+
+            #sol taraftan sagi inme
+            #print("ikinci kisim")
+            noktaX = randint(xSinir - arttirma + 1, merkezX - 1)
+            kontrolx = noktaX
+            kontroly = noktaY
+
+            #print(noktaX,noktaY)
+            xList.append(noktaX)
+            yList.append(noktaY)
+            while True:
+                if (randint(0,99) == 99 and noktaX != kontrolx and noktaY != kontroly) or noktaY-1-((noktaY-1-merkezY)//100+1) < merkezY +1:
+                     break
+
+                noktaX = randint(xSinir - arttirma + 1,merkezX -1)
+                noktaY = randint(noktaY-1-((noktaY-1-merkezY)//100+1),noktaY-1)
+
+                #print(noktaX,noktaY)
+                xList.append(noktaX)
+                yList.append(noktaY)
+        #alta uzanan sekil
+        else:
+            #sol taraftan asagi inme
+            while True:
+                if (randint(0,99) == 99 and noktaX != merkezX and noktaY != merkezY) or noktaY-1-((noktaY-1-(ySinir-arttirma))//100+1) < ySinir - arttirma + 1:
+                    break
+
+                noktaX = randint(xSinir - arttirma + 1, merkezX - 1)
+                noktaY = randint(noktaY-1-((noktaY-1-(ySinir-arttirma))//100+1),noktaY-1)
+
+                #print(noktaX,noktaY)
+                xList.append(noktaX)
+                yList.append(noktaY)
+
+            #sag taraftan yukari cikma
+            #print("ikinci kisim")
+            noktaX = randint(merkezX + 1, xSinir-1)
+            kontrolx = noktaX
+            kontroly = noktaY
+
+            #print(noktaX, noktaY)
+            xList.append(noktaX)
+            yList.append(noktaY)
+            while True:
+                if (randint(0,99) == 99 and noktaX != kontrolx and noktaY != kontroly) or noktaY+1+((merkezY-1-noktaY)//100+1) > merkezY - 1:
+                     break
+
+                noktaX = randint(merkezX + 1,xSinir - 1)
+                noktaY = randint(noktaY+1,noktaY+1+((merkezY-1-noktaY)//100+1))
+                #print(noktaX,noktaY)
+                xList.append(noktaX)
+                yList.append(noktaY)
+
+        #dosyaya datalari yazma
+        #print("data yaziyom")
+        print(len(xList),file=out)
+        for p in range(len(yList)):
+            print(xList[p],yList[p],end=" ",file=out)
+        print(file=out)
+
+        #bir sonraki bolgeye gecme
+        if xSinir + arttirma > sinir:
+            xSinir = arttirma
+            ySinir += arttirma
+        else:
+            xSinir += arttirma
+
+    #roket olusturucu
+
+    print(N,file=out)
+    #print("roket olusturuyom")
+
+    for _ in range(N):
+        roketX = randint(0,sinir)
+        roketY = randint(0,sinir)
+        print(roketX,roketY,file=out)
+
